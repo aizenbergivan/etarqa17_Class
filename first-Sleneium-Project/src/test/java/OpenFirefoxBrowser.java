592e0ff535d9b7641afbae217d7a0808defe5d02
eenat <etarnovskaya@hotmail.com>
2019-01-24 18:26:03 +0200

    24/01/19

--- first-Sleneium-Project/src/test/java/OpenFirefoxBrowser.java
+++ first-Sleneium-Project/src/test/java/OpenFirefoxBrowser.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,11 +13,12 @@ public class OpenFirefoxBrowser {
 
   @BeforeMethod
   public void setUp(){
-    wd =new  FirefoxDriver();
+    wd =new  FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
   }
   @Test
   public void testeBaySearch(){
+    //open site
     wd.get("https://www.ebay.com/");
 
     wd.findElement(By.id("gh-ac")).click();
