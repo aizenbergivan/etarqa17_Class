9b9ccba91cc627461c79633956dbd16e454934f9
eenat <etarnovskaya@hotmail.com>
2019-02-11 19:02:35 +0200

    11/02/19

--- second-selenium-project/src/test/java/manager/ApplicationManager.java
+++ second-selenium-project/src/test/java/manager/ApplicationManager.java
@@ -3,6 +3,10 @@ package manager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,10 +15,24 @@ public class ApplicationManager {
   BoardHelper boardHelper;
   TeamHelper teamHelper;
   WebDriver wd;
+  String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void start() {
-    wd= new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    if (browser.equals(BrowserType.CHROME)) {
+      wd= new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.EDGE)){
+      wd = new EdgeDriver();
+    } else if (browser.equals(BrowserType.IE)){
+      wd = new InternetExplorerDriver();
+    }
+
+    wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     wd.manage().window().maximize();
     openSite("https://trello.com");
     sessionHelper = new SessionHelper(wd);
--- second-selenium-project/src/test/java/manager/HelperBase.java
+++ second-selenium-project/src/test/java/manager/HelperBase.java
@@ -18,7 +18,7 @@ public class HelperBase {
     }
   }
 
-  protected void click(By locator) {
+  public void click(By locator) {
     wd.findElement(locator).click();
   }
 
